d1933c73f39864883fcecc3328f5b4746ca3723e
Sourabh <sourabh.kumar@xebia.com>
2026-03-18 11:19:30 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './App.css'
+import Nav from './navbar';
+import Textform from './textform';
+
+function App() {
+  return(
+    <>
+   <Nav></Nav>
+   <Textform heading={'likho bhai likho'}></Textform>
+   </>
+  
+);
+}
+
+
+export default App
---
+++ src/textform.tsx
@@ -0,0 +1,57 @@
+import { useState, type SetStateAction, } from "react";
+
+type TextformProps = {
+  heading: string;
+};
+
+export default function Textform(props: TextformProps) {
+
+  const [text, setText] = useState(" m amar hu");   // ✅ initialize text
+
+  const handleUpClick = () => {
+    console.log("uppercase was clicked");
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const handleloClick = () => {
+    console.log("lowercase was clicked");
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+
+  const handleChange = (event: { target: { value: SetStateAction<string>; }; }) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <>
+      <div className="container mt-5">
+        <h1>{props.heading}</h1>
+
+        <textarea
+          className="form-control"
+          value={text}
+          onChange={handleChange}
+          rows={8}
+        ></textarea>
+
+        <button className="btn btn-primary mx-3" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary mx-3" onClick={handleloClick}>
+          Convert to lowercase
+        </button>
+      </div>
+      <div className="container my-3">
+        <h1> Summary lele re baba</h1>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+        <p>{0.008 * text.split(" ").length} Minutes</p>
+
+
+      </div>
+
+    </>
+  );
+}
